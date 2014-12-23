59ff58b8e25e72681528d417d7be2c2652112fe4
briofons <briofons@gmail.com>
2014-12-24 00:59:31 +0100

    first lines of R and commit the raw data.

---
+++ lectures/lecture02/example1_heights_data.r
@@ -0,0 +1,14 @@
+##
+## Lecture 02 Height data from lecture
+##
+## @author Alfonso de Uña
+##
+##
+##################################################################################
+
+
+
+#Get data from csv from cs-109
+setwd("C:/Users/Alf/cs109/lectures/lecture02/")
+data <-read.csv(file="Student_Basic_Info.csv", header=TRUE)
+
