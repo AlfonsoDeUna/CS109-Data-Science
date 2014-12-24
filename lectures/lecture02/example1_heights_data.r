2990d45753cda11f4b116db1026fbfa27657ad74
briofons <briofons@gmail.com>
2014-12-24 01:08:50 +0100

    first lines of R 2

--- lectures/lecture02/example1_heights_data.r
+++ lectures/lecture02/example1_heights_data.r
@@ -9,7 +9,7 @@
 
 
 #Get data from csv from cs-109
-setwd("C:/Users/Alf/cs109/lectures/lecture02/")
+setwd("/home/cs109/lectures/lecture02/")
 data <-read.csv(file="Student_Basic_Info.csv", header=TRUE)
 summary(data)
 
