4482441e0fbb8c494b613bc51a1329d9c9bb5ac1
briofons <briofons@gmail.com>
2014-12-24 01:07:39 +0100

    first lines of R

--- lectures/lecture02/example1_heights_data.r
+++ lectures/lecture02/example1_heights_data.r
@@ -1,8 +1,8 @@
+##################################################################################
+## Lecture 02: Height data from lecture...
 ##
-## Lecture 02 Height data from lecture
-##
-## @author Alfonso de Uña
-##
+## @author: Alfonso de Uña
+## @date: 23/12/2014
 ##
 ##################################################################################
 
@@ -11,4 +11,10 @@
 #Get data from csv from cs-109
 setwd("C:/Users/Alf/cs109/lectures/lecture02/")
 data <-read.csv(file="Student_Basic_Info.csv", header=TRUE)
+summary(data)
 
+#We can observe different type of height. It's something normal when we have messy data.
+show(data)
+
+# Transform data. There are different formats in height because we get data in manual way.
+# This kind of transformations is usual when colect data.
